691bf0dfc09cb56b59c51536e00ebb8b7189a0f2
vinayak7N <vinayaknair1996@gmail.com>
2019-12-17 22:20:15 +0530

    Solution for LeetCode problems on Linkedlist

---
+++ LeetCode Solutions/LinkedList/AddTwoNumbers.java
@@ -0,0 +1,62 @@
+package com.LinkedList;
+
+public class AddTwoNumbers {
+
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		ListNode current = null, head = null;
+		int sum = 0, carry = 0;
+		while (l1 != null || l2 != null) {
+			sum = carry;
+			if (l1 != null) {
+				sum += l1.val;
+				l1 = l1.next;
+			}
+			if (l2 != null) {
+				sum += l2.val;
+				l2 = l2.next;
+			}
+
+			ListNode n = new ListNode(sum % 10);
+			if (current == null) {
+				current = n;
+				head = n;
+			} else {
+				current.next = n;
+				current = current.next;
+			}
+			carry = sum / 10;
+		}
+		if (carry == 1) {
+			current.next = new ListNode(1);
+		}
+		return head != null ? head : new ListNode(0);
+	}
+
+	public void displayList(ListNode head) {
+		if (head == null)
+			return;
+		while (head != null) {
+			System.out.print(head.val + " ");
+			head = head.next;
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		AddTwoNumbers singlyLinkedList = new AddTwoNumbers();
+		ListNode l11 = new ListNode(7);
+		ListNode l12 = new ListNode(5);
+		ListNode l13 = new ListNode(8);
+		ListNode l21 = new ListNode(6);
+		ListNode l22 = new ListNode(9);
+		ListNode l23 = new ListNode(2);
+		ListNode l24 = new ListNode(4);
+		l11.next = l12;
+		l12.next = l13;
+		l21.next = l22;
+		l22.next = l23;
+		l23.next = l24;
+		singlyLinkedList.displayList(singlyLinkedList.addTwoNumbers(l11, l21));
+	}
+
+}
---
+++ LeetCode Solutions/LinkedList/RemoveNthFromEnd.java
@@ -0,0 +1,65 @@
+package com.LinkedList;
+
+public class RemoveNthFromEnd {
+
+	
+	public ListNode removeNthFromEnd(ListNode head,int n) {
+		if(head==null)
+			return head;
+		int size=length(head);
+		if(n==size) {
+			System.out.println("deleted node "+head.val);
+			head=head.next;
+			return head;
+		}
+		int pos=size-n;
+		int i=0;
+		ListNode prev=null,current=head;
+		while(i<pos) {
+			prev=current;
+			current=current.next;
+			i++;
+		}
+		System.out.println("Deleted node "+current.val);
+		prev.next=current.next;
+		current.next=null;
+		return head;
+	}
+	
+	public void displayList(ListNode head) {
+		if(head==null)
+			return;
+		while(head!=null) {
+			System.out.print(head.val+" ");
+			head=head.next;
+		}
+		System.out.println();
+	}
+	
+	private int length(ListNode head) {
+		if(head==null)
+			return 0;
+		int count=0;
+		while(head!=null) {
+			head=head.next;
+			count++;
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+		RemoveNthFromEnd singlyLinkedList=new RemoveNthFromEnd();
+		ListNode head=new ListNode(10);
+		ListNode second=new ListNode(20);
+		ListNode third=new ListNode(30);
+		ListNode fourth=new ListNode(40);
+		ListNode fifth=new ListNode(50);
+		head.next=second;
+		second.next=third;
+		third.next=fourth;
+		fourth.next=fifth;
+		System.out.println(head);
+		singlyLinkedList.displayList(singlyLinkedList.removeNthFromEnd(head, 5));
+	}
+
+}
