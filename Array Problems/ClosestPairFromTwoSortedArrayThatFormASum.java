06d6acf1ef576431f7d73f2e71ee9224c7da08ad
Vinayak Nair <vinayaknair1996@gmail.com>
2020-05-19 12:30:07 +0530

    Closet pair to a sum from two sorted array problem

--- Array Problems/ClosestPairFromTwoSortedArrayThatFormASum.java
+++ Array Problems/ClosestPairFromTwoSortedArrayThatFormASum.java
@@ -2,12 +2,12 @@ public class ClosestPairFromTwoSortedArrayThatFormASum {
 
     public static int[] closetPairSum(int[] first,int[] second,int sum){
 
-        int diff=sum-first[0]+second[0];
+        int diff=Integer.MAX_VALUE;
         int[] pair=new int[2];
         for(int i=0;i<first.length;i++){
-            for(int j=1;j<second.length;j++){
-                int x=sum-(first[i]+second[j]);
-                if(x>0 && x<diff) {
+            for(int j=0;j<second.length;j++){
+                int x=Math.abs(sum-(first[i]+second[j]));
+                if(x<diff) {
                     diff = x;
                     pair[0]=first[i];
                     pair[1]=second[j];
@@ -16,10 +16,31 @@ public class ClosestPairFromTwoSortedArrayThatFormASum {
         }
         return pair;
     }
+
+    public static int[] closetPairSumUsingTwoPointer(int[] first,int[] second,int sum){
+
+        int[] pair=new int[2];
+        int diff=Integer.MAX_VALUE;
+        int l=0,r=second.length-1;
+        while(l<first.length && r>=0){
+
+            int x=Math.abs(sum-(first[l]+second[r]));
+            if(x<diff){
+                diff=x;
+                pair[0]=first[l];
+                pair[1]=second[r];
+            }
+            if(first[l]+second[r]>x)
+                r--;
+            else
+                l++;
+        }
+        return pair;
+    }
     public static void main(String[] args) {
-        int[] first={1,4,5,7};
+        int[] first={1,3,5,7};
         int[] second={10,20,30,40};
-        int[] array=closetPairSum(first,second,50);
+        int[] array=closetPairSum(first,second,32);
         System.out.println("Closet pairs: "+array[0]+" and "+array[1]);
     }
 }
