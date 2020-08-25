d7cdd79e08dee2f04dcec2b1996afc783faf066d
Vinayak Nair <vinayaknair1996@gmail.com>
2020-08-25 16:17:03 +0530

    ThreadLocal class example

--- MultithreadingPrograms/src/com/example16/ThreadLocalDemo1.java
+++ MultithreadingPrograms/src/com/example16/ThreadLocalDemo1.java
@@ -7,14 +7,31 @@ public class ThreadLocalDemo1 {
         ThreadLocal t1 = new ThreadLocal();
 
         //Default initial value = null
-        System.out.println(t1.get());
+        System.out.println("T1:" + t1.get());
 
         t1.set("MyValue");
-        System.out.println(t1.get());
+        System.out.println("T1:" + t1.get());
         t1.remove();
 
         //Default initial value = null
-        System.out.println(t1.get());
+        System.out.println("T1:" + t1.get());
+
+        //Overriding initial value using anonymous class
+        ThreadLocal t2 = new ThreadLocal() {
+
+            public Object initialValue() {
+                return "Default Value";
+            }
+        };
+        //Default initial value = Default Value
+        System.out.println("T2:" + t2.get());
+
+        t2.set("MyValue");
+        System.out.println("T2:" + t2.get());
+        t2.remove();
+
+        //Default initial value = Default Value
+        System.out.println("T2:" + t2.get());
     }
 
 }
